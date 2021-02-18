3e5acca3b08f05d9cd2ef74663ac4107b9027760
Thanh Dao <thanhdd1@gmail.com>
2021-02-18 17:57:46 +0900

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # byebug
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = []
+    if params[:ratings] != nil
+      @ratings_to_show = params[:ratings].keys
+    end
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    Movie.select('DISTINCT rating').map(&:rating).sort #order('rating ASC')
+  end
+
+  def self.with_ratings(ratings_list)
+    return Movie.all if ratings_list == nil || ratings_list.size == 0
+    Movie.where("rating IN (?)", ratings_list)
+  end
 end
